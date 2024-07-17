7aef7f6620ec8e392e3e5dd22fb0551ecf181d88
EstifanosTaye320 <estifanostaye11@gmail.com>
2024-07-17 10:54:34 +0300

    seting up files for the ui

---
+++ ui/scripts/models/todoList.js
@@ -0,0 +1,22 @@
+const TodoItem = require("./todoItem");
+
+function TodoList() {
+  this.todos = [];
+  this.addtodo = function (title, message) {
+    const new_todo = new TodoItem();
+    new_todo.message = message;
+    new_todo.title = title;
+    this.todos.push(new_todo);
+  };
+  this.update = function (idx, title = null, message = null, callback) {
+    try {
+      const to_update = this.todos[idx];
+      to_update.update(title, message);
+      callback(null, to_update);
+    } catch (err) {
+      callback(err, null);
+    }
+  };
+}
+
+module.exports = TodoList;
